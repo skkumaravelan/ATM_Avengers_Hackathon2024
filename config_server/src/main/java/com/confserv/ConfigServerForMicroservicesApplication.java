8960c89ee8acf1e59ff3534156027b5cba879d98
skkumaravelan <89237419+skkumaravelan@users.noreply.github.com>
2024-07-30 13:16:03 +0100

    Added 8002

---
+++ config_server/src/main/java/com/confserv/ConfigServerForMicroservicesApplication.java
@@ -0,0 +1,15 @@
+package com.confserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerForMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerForMicroservicesApplication.class, args);
+	}
+
+}
---
+++ config_server/src/test/java/com/confserv/ConfigServerForMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerForMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
